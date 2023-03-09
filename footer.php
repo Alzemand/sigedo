e39ad0e3a6c58e407cd4e4f96b52acf272ee999b
alzemand <alzemand@outlook.com>
2023-03-08 22:14:40 -0300

    Removido caminhos não usados

--- footer.php
+++ footer.php
@@ -20,16 +20,16 @@ require_once 'functions/config.php';
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="<?=RAIZ?>assets/vendor/apexcharts/apexcharts.min.js"></script>
-  <script src="<?=RAIZ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="<?=RAIZ?>assets/vendor/chart.js/chart.umd.js"></script>
-  <script src="<?=RAIZ?>assets/vendor/echarts/echarts.min.js"></script>
-  <script src="<?=RAIZ?>assets/vendor/quill/quill.min.js"></script>
-  <script src="<?=RAIZ?>assets/vendor/simple-datatables/simple-datatables.js"></script>
-  <script src="<?=RAIZ?>assets/vendor/tinymce/tinymce.min.js"></script>
-  <script src="<?=RAIZ?>assets/vendor/php-email-form/validate.js"></script>
-  <script src="<?=RAIZ?>assets/vendor/jquery/jquery-3.6.3.min.js"></script>
+  <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/chart.js/chart.umd.js"></script>
+  <script src="assets/vendor/echarts/echarts.min.js"></script>
+  <script src="assets/vendor/quill/quill.min.js"></script>
+  <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/jquery/jquery-3.6.3.min.js"></script>
   
   <!-- Template Main JS File -->
-  <script src="<?=RAIZ?>assets/js/jquery.mask.min.js"></script>
-  <script src="<?=RAIZ?>assets/js/main.js"></script>
+  <script src="assets/js/jquery.mask.min.js"></script>
+  <script src="assets/js/main.js"></script>
--- head.php
+++ head.php
@@ -13,24 +13,24 @@ require_once 'functions/config.php';
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="<?=RAIZ?>assets/img/logo.png" rel="icon">
-  <link href="<?=RAIZ?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="assets/img/logo.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="<?=RAIZ?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="<?=RAIZ?>assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="<?=RAIZ?>assets/vendor/quill/quill.snow.css" rel="stylesheet">
-  <link href="<?=RAIZ?>assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-  <link href="<?=RAIZ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="<?=RAIZ?>assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="<?=RAIZ?>assets/vendor/simple-datatables/style.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="<?=RAIZ?>assets/css/style.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: NiceAdmin - v2.5.0
